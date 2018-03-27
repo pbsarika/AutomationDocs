261b5e31ce4dab4adc90bf93b2c7e30f2b1b9f66
lenovo <lenovo@192.168.0.4>
2018-03-27 22:00:14 +0530

    first push

---
+++ automation/src/autoday1/Launch.java
@@ -0,0 +1,15 @@
+package autoday1;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i= 10;
+		int j = 20;
+		int add = i+j;
+		System.out.println(add);
+		
+
+	}
+
+}
